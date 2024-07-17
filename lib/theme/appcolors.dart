8babbe6027f24bb084bead6caee4eac13a3cb8a6
Guest User <guest.user@finouxad.com>
2024-07-17 11:36:59 +0530

    Project Setup and Essential File Structured

---
+++ lib/theme/appcolors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class Appcolors{
+  static const Color primary=Color.fromRGBO(246, 113, 240, 1.0);
+  static const Color green =Color.fromRGBO(0, 188, 81, 1.0);
+  static const Color blue=Color.fromRGBO(0, 188, 182, 1.0);
+  static const Color black=Color.fromRGBO(64, 64, 64, 1.0);
+  static const Color white=Color.fromRGBO(255, 255, 255, 1.0);
+
+
+
+}
